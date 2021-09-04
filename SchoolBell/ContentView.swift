033434d26d94cca1fb484b20c72721bc7d57e6be
Taehee Kang <dev.thk28@gmail.com>
2021-09-04 18:36:56 +0900

    Initial Commit

---
+++ SchoolBell/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SchoolBell
+//
+//  Created by 강태희 on 2021/09/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SchoolBell/SchoolBellApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchoolBellApp.swift
+//  SchoolBell
+//
+//  Created by 강태희 on 2021/09/04.
+//
+
+import SwiftUI
+
+@main
+struct SchoolBellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
